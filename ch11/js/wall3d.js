af2aa6b4a6716d0379a066cccb649067f6e1a071
rossa <fashionfish@naver.com>
2021-08-31 20:18:40 +0900

    11장 전진! 3D 스크롤 9

--- ch11/js/wall3d.js
+++ ch11/js/wall3d.js
@@ -1,9 +1,16 @@
 (function () {
   const houseElem = document.querySelector('.house');
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
   window.addEventListener('scroll', function () {
     const zMove = (window.pageYOffset / maxScrollValue) * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`;
   });
+
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
 })();
